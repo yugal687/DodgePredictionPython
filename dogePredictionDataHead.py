4c03684ba495bcb48c90eeb6d51531a6e5f1ffbc
yugal687 <pageniyugal@gmail.com>
2024-08-21 10:50:01 -0500

    added virtual environment

--- dogePredictionDataHead.py
+++ dogePredictionDataHead.py
@@ -3,9 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from seaborn import regression
+import sys
+
+
 sns.set()
 # print(plt.style.available)
 plt.style.use('seaborn-v0_8-white')
 
 data = pd.read_csv("DOGE-USD.csv")
-print(data.head())
+# print(data.head())
+print(sys.executable)
