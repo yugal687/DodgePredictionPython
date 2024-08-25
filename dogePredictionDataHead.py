865e02f74a4945484e7b12b733f4433c68e9a138
yugal687 <pageniyugal@gmail.com>
2024-08-25 00:25:46 -0500

    Visualization modules added

--- dogePredictionDataHead.py
+++ dogePredictionDataHead.py
@@ -3,13 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from seaborn import regression
-import sys
+# import sys
 
 
 sns.set()
-# print(plt.style.available)
+print(plt.style.available)
 plt.style.use('seaborn-v0_8-white')
 
 data = pd.read_csv("DOGE-USD.csv")
-# print(data.head())
-print(sys.executable)
+print(data.head())
+# print(sys.executable)
--- dogePredictionFuture.py
+++ dogePredictionFuture.py
@@ -3,6 +3,7 @@ from autots import AutoTS
 import matplotlib.pyplot as plt
 
 
+
 model = AutoTS(forecast_length=2, frequency='infer', ensemble='simple', drop_data_older_than_periods=200)
 model = model.fit(data, date_col='Date', value_col='Close', id_col=None)
 
